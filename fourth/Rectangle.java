270cb975af9c47f51bc9f938345ba61ad1bb1f2d
julia-888 <71026631+julia-888@users.noreply.github.com>
2020-09-13 19:18:24 +0300

    Update Rectangle.java

--- fourth/Rectangle.java
+++ fourth/Rectangle.java
@@ -1,37 +1,37 @@
 package fourth;
 
 public class Rectangle extends Shape{
-    double width;
-    double length;
-    Rectangle(){}
-    Rectangle(double length, double width){
+    protected double width;
+    protected double length;
+    public Rectangle(){}
+    public Rectangle(double length, double width){
         this.length = length;
         this.width = width;
     }
-    Rectangle(double length, double width, String color, boolean filled){
+    public Rectangle(double length, double width, String color, boolean filled){
         this.length = length;
         this.width = width;
         this.color = color;
         this.filled = filled;
     }
 
-    double getWidth(){
+    public double getWidth(){
         return width;
     }
-    double getLength(){
+    public double getLength(){
         return length;
     }
-    void setWidth(double width){
+    public void setWidth(double width){
         this.width = width;
     }
-    void setLength(double length){
+    public void setLength(double length){
         this.length = length;
     }
 
-    double getArea(){
+    public double getArea(){
         return width*length;
     }
-    double getPerimeter(){
+    public double getPerimeter(){
         return 2*(width+length);
     }
     public String toString(){

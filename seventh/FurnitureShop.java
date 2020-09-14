50f8ac624a20114acd25637e32a72749f19cfa48
julia-888 <71026631+julia-888@users.noreply.github.com>
2020-09-14 15:08:41 +0300

    Add files via upload

---
+++ seventh/FurnitureShop.java
@@ -0,0 +1,19 @@
+package seventh;
+
+import java.util.ArrayList;
+
+public class FurnitureShop {
+    ArrayList<Furniture> catalog;
+    FurnitureShop(){
+        catalog  = new ArrayList<>();
+    }
+
+    void Add(Furniture x){
+        catalog.add(x);
+    }
+
+    String getFurniture(int i){
+        return catalog.get(i).toString();
+    }
+
+}

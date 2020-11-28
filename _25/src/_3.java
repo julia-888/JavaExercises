05057de458eb5c6a47eea59d49ed9a0273624f19
julia-888 <71026631+julia-888@users.noreply.github.com>
2020-11-28 13:02:02 +0300

    Update _3.java

--- _25/src/_3.java
+++ _25/src/_3.java
@@ -5,7 +5,7 @@ import java.util.regex.Pattern;
 public class _3 {
     public static void main(String[] args) {
         String str = new Scanner(System.in).nextLine();
-        String pattern = "((2[0-5][0-5]|1[0-9]{2}|[1-9][0-9]|[0-9])\\.){3}(2[0-5][0-5]|1[0-9]{2}|[1-9][0-9]|[0-9])";
+        String pattern = "((25[0-5]|2[0-4][0-9]|1[0-9]{2}|[1-9][0-9]|[0-9])\\.){3}(2[0-5][0-5]|1[0-9]{2}|[1-9][0-9]|[0-9])";
 
         Pattern r = Pattern.compile(pattern);
         Matcher m = r.matcher(str);
